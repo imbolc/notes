6c371cf8f218a4a268908ca94ff5b0a2e526900f
imbolc <me@imbolc.name>
2023-12-16 03:07:49 +0600

    Up

--- pg/csv-dump.md
+++ pg/csv-dump.md
@@ -1,11 +1,11 @@
 # Postgres csv dump
 
 ```sql
-\copy (table my_table) to './my_table.csv' CSV;
+\copy (table my_table) TO './my_table.csv' CSV;
 ```
 
 Copy table in `tsv` format with header:
 
 ```sql
-\copy (SELECT * FROM my_table ORDER BY id) to './my_table.tsv' DELIMITER '\t' CSV HEADER;
+\copy (SELECT * FROM my_table ORDER BY id) TO './my_table.tsv' DELIMITER E'\t' CSV HEADER;
 ```
--- rs/sqlx-enum.rs
+++ rs/sqlx-enum.rs
@@ -7,8 +7,7 @@
 use sqlx::Row;
 
 #[derive(Debug, PartialEq, sqlx::Type)]
-#[sqlx(type_name = "color")]
-#[sqlx(rename_all = "lowercase")]
+#[sqlx(type_name = "color", rename_all = "lowercase")]
 enum Color {
     Red,
     Green,
@@ -21,10 +20,10 @@ async fn main() -> sqlx::Result<()> {
     let db = sqlx::PgPool::connect(&std::env::var("DATABASE_URL").unwrap()).await?;
     let mut tx = db.begin().await?;
 
-    sqlx::query("CREATE TYPE color AS ENUM ('red', 'green', 'blue')")
+    sqlx::query!("CREATE TYPE color AS ENUM ('red', 'green', 'blue')")
         .execute(&mut *tx)
         .await?;
-    sqlx::query("CREATE TABLE colors (color color)")
+    sqlx::query!("CREATE TABLE colors (color color)")
         .execute(&mut *tx)
         .await?;
     sqlx::query("INSERT INTO colors VALUES ($1), ($2)")
@@ -33,24 +32,35 @@ async fn main() -> sqlx::Result<()> {
         .execute(&mut *tx)
         .await?;
 
-    // Quering by a single enum works
+    // The macro won't compile as we create table on the fly,
+    // but in RL the explicit typing `Color::Red as _` is necessary
+    // sqlx::query!("INSERT INTO colors VALUES ($1)", Color::Red as _)
+    //     .execute(&mut *tx)
+    //     .await?;
+
+    // Querying by a single enum works
     assert_eq!(
         sqlx::query("SELECT color FROM colors WHERE color = $1")
             .bind(Color::Red)
             .fetch_one(&mut *tx)
             .await?
-            .get::<Color, &str>("color"),
+            .get::<Color, _>("color"),
         Color::Red
     );
 
+    // But for querying by an array we have to implement this trait
+    impl sqlx::postgres::PgHasArrayType for Color {
+        fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+            sqlx::postgres::PgTypeInfo::with_name("_color")
+        }
+    }
+
     assert_eq!(
         sqlx::query("SELECT color FROM colors WHERE color = ANY($1::color[])")
-            // Error: the trait `PgHasArrayType` is not implemented for `Color`
-            // .bind(&[Color::Blue, Color::Red])
-            .bind(&["blue", "red"])
+            .bind(&[Color::Blue, Color::Red])
             .fetch_one(&mut *tx)
             .await?
-            .get::<Color, &str>("color"),
+            .get::<Color, _>("color"),
         Color::Red
     );
 
